78d5d7e6d297eb182fee745616bff3021d7ddf7e
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2022-10-10 17:16:24 +0800

    feat: Dialogue&UI&SoundMgr&somefix

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+
+
+[Serializable]
+public class Dialogue
+{
+    public Dialogue(int index, string person, string dialogueText)
+    {
+        this.index = index;
+        this.nameText = person;
+        this.person = Enum.Parse<Person>(person);
+        this.dialogueText = dialogueText;
+    }
+    public enum Person
+    {
+        Eve,
+        Cosette
+    }
+    public Person person;
+    //public int face;
+    public int index;
+    public string nameText;
+    public string dialogueText;
+    
+}
--- Assets/Scripts/Ultis/Singleton/SingletonAutoMono.cs
+++ Assets/Scripts/Ultis/Singleton/SingletonAutoMono.cs
@@ -10,6 +10,15 @@ public class SingletonAutoMono<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
 
+    protected virtual void Awake()
+    {
+        if (GetInstance()!=this)
+        {
+            Destroy(this);
+        }
+        else DontDestroyOnLoad(this);
+    }
+
     public static T GetInstance()
     {
         if (!instance)//如果单例不存在,则自动生成一个空物体,并自动将脚本挂上,并返回
@@ -17,7 +26,6 @@ public class SingletonAutoMono<T> : MonoBehaviour where T : MonoBehaviour
             GameObject gameObject = new GameObject(typeof(T).ToString()) ;
             instance = gameObject.AddComponent<T>();
         }
-
         return instance;
     }
 
